e3dd11ac8562ac1d2675f6ac8e91347a16c9cd49
Leo Tu <limintu@MacBook-Pro.local>
2019-08-13 10:56:08 -0700

    refactor test.py and solution.py

--- Daily Interview Pro/Longest Substring Without Repeating Characters/solution.py
+++ Daily Interview Pro/Longest Substring Without Repeating Characters/solution.py
@@ -1,15 +1,16 @@
-def lengthOfLongestSubstring(s: str) -> int:
-    i, j = 0, 0
-    count = {}
-    ans = 0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        i, j = 0, 0
+        count = {}
+        ans = 0
 
-    while j < len(s):
-        c = s[j]
-        if c in count:
-            i = max(i, count[c] + 1)
+        while j < len(s):
+            c = s[j]
+            if c in count:
+                i = max(i, count[c] + 1)
 
-        ans = max(ans, j - i + 1)
-        count[c] = j
-        j += 1
+            ans = max(ans, j - i + 1)
+            count[c] = j
+            j += 1
 
-    return ans
+        return ans
--- Daily Interview Pro/Longest Substring Without Repeating Characters/test.py
+++ Daily Interview Pro/Longest Substring Without Repeating Characters/test.py
@@ -1,19 +1,19 @@
 import unittest
-from solution import lengthOfLongestSubstring
+from solution import Solution
 
 
 class MyTestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(lengthOfLongestSubstring("abrkaabcdefghijjxxx"), 10)
+        self.assertEqual(Solution().lengthOfLongestSubstring("abrkaabcdefghijjxxx"), 10)
 
     def test2(self):
-        self.assertEqual(lengthOfLongestSubstring('abcabcbb'), 3)
+        self.assertEqual(Solution().lengthOfLongestSubstring('abcabcbb'), 3)
 
     def test3(self):
-        self.assertEqual(lengthOfLongestSubstring('bbbbb'), 1)
+        self.assertEqual(Solution().lengthOfLongestSubstring('bbbbb'), 1)
 
     def test4(self):
-        self.assertEqual(lengthOfLongestSubstring('pwwkew'), 3)
+        self.assertEqual(Solution().lengthOfLongestSubstring('pwwkew'), 3)
 
 
 if __name__ == '__main__':
